a1dadda703ea0a8e3ae87ff7a78588276b97e8b1
Prakasa Judha <prakasajudha@gmail.com>
2022-03-12 15:29:15 +0700

    homepage before function

--- src/apis/food.js
+++ src/apis/food.js
@@ -4,3 +4,7 @@ import { API_BASE_URL } from '../constants/defaultValues';
 export const getRecipes = () =>{
   return axios.get(API_BASE_URL + '/recipes')
 }
+
+export const getCategoryFood = () =>{
+  return axios.get(API_BASE_URL + '/recipe-categories')
+}
--- src/page/homePage/HomePage.js
+++ src/page/homePage/HomePage.js
@@ -7,16 +7,23 @@ import HeaderLogo from '../../assets/logo/header-logo.png'
 import HistoryPNG from '../../assets/others/historyPNG.png'
 import {Input} from "antd";
 import { useNavigate } from 'react-router-dom';
-import {getRecipes} from "../../apis/food";
+import {getCategoryFood, getRecipes} from "../../apis/food";
 import FoodCard from "../../component/card/FoodCard";
 
 const HomePage = (props) => {
   const [foodData, setFoodData] = useState([])
+  const [categoryFoodData, setCategoryFoodData] = useState([{
+    id: 0,
+    name: 'Semua',
+  }])
   const [load, setLoad] = useState(false)
+  const [currentSort, setCurrentSort] = useState('new')
+  const [currentCategory, setCurrentCategory] = useState(0)
   let navigate = useNavigate()
 
   useEffect(() => {
     getFoodData()
+    getCategoryFoodData()
   }, []);
 
   const getFoodData = async() =>{
@@ -32,13 +39,23 @@ const HomePage = (props) => {
     }
   }
 
+  const getCategoryFoodData = async() =>{
+    try{
+      let response = await getCategoryFood()
+      let categoryData = response.data.data
+      setCategoryFoodData(categoryData)
+    }catch(e){
+      message.error('Error when fetching category data')
+    }
+  }
+
   const handleNavigateToHomePage = () =>{
     navigate('/code-food-homepage')
   }
 
   const renderHeader = () =>{
     return <div className={'homepage-header-wrapper'}>
-      <Row style={{height:'100%'}}>
+      <Row style={{height:'65%'}}>
         <Col style={{margin:'auto'}}>
           <div>
             <img src={HeaderLogo} data-cy={"header-logo"} onClick={() => handleNavigateToHomePage()} style={{cursor:'pointer'}}/>
@@ -51,11 +68,35 @@ const HomePage = (props) => {
         </Col>
         <Col style={{margin:'auto'}}>
           <div>
-            <img style={{border:'1px solid #EAEAEA', borderRadius:'5px', padding:'3px', cursor:'pointer'}} src={HistoryPNG} data-cy={"header-logo"}/>
+            <img style={{border:'1px solid #EAEAEA',textAlign:'center', borderRadius:'5px', padding:'3px', cursor:'pointer'}} src={HistoryPNG} data-cy={"header-logo"}/>
           </div>
         </Col>
       </Row>
+      {handleRenderCategoryButton()}
     </div>
+  }
+
+  const handleRenderCategoryButton = () =>{
+    return <div className={'homepage-header-category-wrapper'}>
+      {categoryFoodData.map((item,index) => { return renderCategoryButton(item, index)})}
+    </div>
+  }
+
+  const renderCategoryButton = (item, index) =>{
+    return <div className={'homepage-header-category'}>
+      <Button className={'homepage-header-category-button ' + renderStyleActiveCategoryButton(item.id)} onClick={() => handleSortCategory(item.id)}data-cy={'category-button' + index}>{item.name}</Button>
+    </div>
+  }
+
+  const renderStyleActiveCategoryButton = (id) => {
+    if (currentCategory == id) {
+      return 'category-active'
+    }
+    return 'category-non-active'
+  }
+
+  const handleSortCategory =(id) =>{
+    setCurrentCategory(id)
   }
 
   const handleRenderContent = () =>{
@@ -73,6 +114,44 @@ const HomePage = (props) => {
     </div>
   }
 
+  const handleSort = (sortBy) =>{
+    setCurrentSort(sortBy)
+  }
+
+  const renderStyleActiveSort = (code) =>{
+    if(code == currentSort){
+      return 'active-button-sort'
+    }
+    return 'not-active-button-sort'
+  }
+
+  const renderSortButton = () =>{
+    let sortButton =[
+      {
+        name:'Terbaru',
+        code:'new',
+        dataCy:'button-sort-latest'
+      },{
+        name:'Urutkan A-Z',
+        code:'asc',
+        dataCy:'button-sort-az'
+      },{
+        name:'Urutkan Z-A',
+        code:'desc',
+        dataCy:'button-sort-za'
+      },{
+        name:'Urutkan Dari Paling Disukai',
+        code:'liked',
+        dataCy:'button-sort-favorite'
+
+      },
+    ]
+
+    return sortButton.map(item => {return <Button className={renderStyleActiveSort(item.code)} style={{margin:'0 10px'}} onClick={() =>handleSort(item.code)}>
+      {item.name}
+    </Button>})
+  }
+
 
   return (
     <div className={'homepage'}>
@@ -81,6 +160,12 @@ const HomePage = (props) => {
       </div>
       <div style={{backgroundColor:'#E5E5E5'}}>
         <div className={'homepage-body'}>
+          <div>
+            <div style={{display:'flex', alignItems:'center', marginBottom:'20px'}}>
+              <p style={{fontFamily:'Poppins', marginBottom:'0',fontWeight:'bold', fontSize:'16px', lineHeight:'24px'}}>Urutkan:</p>
+              {renderSortButton()}
+            </div>
+          </div>
           {load ? renderSpinner() : <div className={'homepage-body-cards'}>
             {handleRenderContent()}
           </div>}
