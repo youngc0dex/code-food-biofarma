4b6f6a89c54578b59cc78304556afbb469cb5d61
Prakasa Judha <prakasajudha@gmail.com>
2022-03-13 15:05:15 +0700

    change public env

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ import RecipePage from "./page/recipePage/RecipePage";
 import './index.css'
 function App(props) {
   return (
-    // <Router basename={process.env.PUBLIC_URL}>
-    <Router >
+    <Router basename={process.env.PUBLIC_URL}>
+    {/*<Router >*/}
       <Routes>
           <Route exact path='/' element={<Homepage/>} />
           <Route path='/login' element ={<Login/>}/>
