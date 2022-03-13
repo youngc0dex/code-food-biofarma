e9e2c870bb1d9145614bf59f6328545963fc7bda
Prakasa Judha <prakasajudha@gmail.com>
2022-03-14 01:40:38 +0700

    update

--- src/apis/auth/token.js
+++ src/apis/auth/token.js
@@ -4,7 +4,7 @@ export class TokenService {
     localStorage.setItem(key,token)
   }
   static getToken(){
-    return localStorage.getItem("Food-Token") || "";
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDcxOTUxMDgsInN1YiI6IjEifQ.SJmSoECYWYyGkD4F6O2PjTskk2TlZLcj2gStt8_2ivA'
   }
   static getheader(){
     let headers = {
